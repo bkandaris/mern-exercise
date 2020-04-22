bc9544f090e130a52ce9037e03c1243563d78e92
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-04-22 02:16:45 -0400

    basic component structure complete

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
+import CreateExercise from './components/CreateExercise';
+import CreateUser from './components/CreateUser';
+import EditExercise from './components/EditExercise';
+import ExerciseList from './components/ExerciseList';
+
 function App() {
   return (
-    <Router>
-      Hello World
-      {/* <Navbar />
-      <br />
-      <Route path='/' exact component={ExercisesList} />
-      <Route path='/edit/:id' component={EditExercise} />
-      <Route path='/create' component={CreateExercise} />
-      <Route path='/user' component={CreateUser} /> */}
-    </Router>
+    <div>
+      <Router>
+        <div className='container'>
+          <Navbar />
+          <br />
+          <Route path='/' exact component={ExerciseList} />
+          <Route path='/edit/:id' component={EditExercise} />
+          <Route path='/create' component={CreateExercise} />
+          <Route path='/user' component={CreateUser} />
+        </div>
+      </Router>
+    </div>
   );
 }
 
---
+++ mern-exercise-tracker/src/components/CreateExercise.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CreateExercise(props) {
+  return <div>Create Exercise Component</div>;
+}
